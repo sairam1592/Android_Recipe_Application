3fe377233e6e083a4958efc17fda5682f0ff88b5
Arun Sairam Ashok <sairam.a1592@gmail.com>
2024-03-03 19:17:14 +0800

    feat: add no internet view and code to reload content

--- app/src/main/java/com/example/myapplication/arunproject/presentation/view/compose/ErrorScreen.kt
+++ app/src/main/java/com/example/myapplication/arunproject/presentation/view/compose/ErrorScreen.kt
@@ -1,36 +1,67 @@
 package com.example.myapplication.arunproject.presentation.view.compose
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.sp
 import com.arun.myapplication.R
 import com.example.myapplication.arunproject.common.AppConstants
+import com.example.myapplication.arunproject.common.AppConstants.RETRY
 
 /**
  * Show error screen
  */
 @Composable
-fun ErrorScreen() {
+fun ErrorScreen(onRetry: () -> Unit) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(dimensionResource(id = R.dimen.padding_16)),
-        contentAlignment = Alignment.Center
+            .padding(dimensionResource(id = R.dimen.padding_20)),
+        contentAlignment = Alignment.CenterStart
     ) {
-        Column {
-            Text(
-                AppConstants.ERROR_MESSAGE,
-                color = Color.Red,
-                fontSize = dimensionResource(id = R.dimen.text_size_18).value.sp
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_error_img),
+                contentDescription = "",
+                modifier = Modifier
+                    .wrapContentSize(),
             )
+
+            Spacer(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_10)))
+
+            Text(
+                AppConstants.NO_INTERNET_ERROR_MESSAGE,
+                color = colorResource(id = R.color.blue_primary),
+                fontSize = dimensionResource(id = R.dimen.text_size_large).value.sp,
+                fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
+                textAlign = androidx.compose.ui.text.style.TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_10)))
+
+            Button(
+                onClick = { onRetry() },
+                colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(id = R.color.blue_primary))
+            ) {
+                Text(
+                    text = RETRY,
+                    color = colorResource(id = R.color.white),
+                )
+            }
         }
     }
 }
